c6c09419a1499c6df885850727699fe3f8082080
kotaro1574 <shake1574@gmail.com>
2021-07-14 10:03:48 +0900

    Sign up機能

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyA8Z7r-uUifizNtIxWN8iJ4xZCBH6GEUmI",
+  authDomain: "ec-react-bdb1d.firebaseapp.com",
+  projectId: "ec-react-bdb1d",
+  storageBucket: "ec-react-bdb1d.appspot.com",
+  messagingSenderId: "649605913011",
+  appId: "1:649605913011:web:be1f2b1efb9b7fa60853c6",
+  measurementId: "G-Z455J16NM7"
+};
