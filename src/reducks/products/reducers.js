ab6219ed32b8776a3e1be8f733f59c213a71f842
kotaro1574 <shake1574@gmail.com>
2021-07-23 08:36:20 +0900

    product edit作成

---
+++ src/reducks/products/reducers.js
@@ -0,0 +1,9 @@
+import * as Actions from './actions'
+import initialState from '../store/initalState'
+
+export const ProductsReducer = (state = initialState.products, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
--- src/reducks/store/store.js
+++ src/reducks/store/store.js
@@ -3,18 +3,26 @@ import {
   combineReducers,
   applyMiddleware,
 } from 'redux'
+import {createLogger} from 'redux-logger/src'
 import { connectRouter, routerMiddleware } from 'connected-react-router'
 import thunk from 'redux-thunk'
 
 import { UsersReducer } from '../users/reducers'
+import { ProductsReducer } from '../products/reducers'
 
 export default function createStore(history) {
+  const logger = createLogger({
+    collapsed: true,
+    diff: true
+  })
   return reduxCreateStore( // reduxのcreateStoreメソッドの別名
     combineReducers({
+      products: ProductsReducer,
       router: connectRouter(history),
       users: UsersReducer,
     }),
     applyMiddleware(
+      logger,
       routerMiddleware(history),
       thunk
     )
