0125e968891018a0b0d448e04e20de1df791b091
kotaro1574 <shake1574@gmail.com>
2021-09-06 08:44:06 +0900

    ヘッダー作成

--- src/templates/ProductDetail.jsx
+++ src/templates/ProductDetail.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { db } from '../firebase/index'
 import { makeStyles } from '@material-ui/core/styles' 
